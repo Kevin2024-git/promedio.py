4a0b4b74dac38b1b33826d6dc1bde9beae83d841
Kevin2024-git <kevin.zambrano@tecnologicoliceocristiano.edu.ec>
2024-07-23 19:31:32 -0500

    PromedioPython

---
+++ 01-Numeros.py
@@ -0,0 +1,20 @@
+# Escriba un programa que solicite al usuario ingresar tres numeros 
+# para luego mostrarle el promedio de los tres.
+print("")
+print("ingrese los tres numeros")
+
+N1 = int(input("ingrese el primer numero: "))
+print("")
+N2 = int(input("ingresa el segundo numero: "))
+print("")
+N3 = int(input("ingresa el trecer numero: "))
+print("")
+
+S = (N1 + N2+ N3)
+
+P = (S)/3
+
+print("El promedio es de: ")
+print(P)
+print("")
+
